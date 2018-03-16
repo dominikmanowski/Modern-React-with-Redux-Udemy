ec93d7e59d0c64c9f181bc5e9c3c6444addbffcb
Dominik Manowski <manowski@protomail.com>
2018-03-16 16:13:10 +0100

    add onVideoSelected callback

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from "./components/video_detail";
 
 
 const API_KEY = 'AIzaSyDspbD9Gw7mxPFsaYbHFVkyokOGmZeKbTQ';
@@ -11,13 +12,20 @@ const API_KEY = 'AIzaSyDspbD9Gw7mxPFsaYbHFVkyokOGmZeKbTQ';
 
 class App extends Component {
 
+
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'bass guitar'}, (videos) => {
-            this.setState({ videos })
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         });
     }
 
@@ -25,7 +33,10 @@ class App extends Component {
         return (
             <React.Fragment>
                 <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </React.Fragment>
         )
     }
